4925b2eddd1ebf461acb93f0348d8528c3280b15
Viktor Varenik <yavarenikya@gmail.com>
2024-10-17 19:08:42 +0000

    Feature/prepare gradle (#2)
    * chore(app/gradle): enable minify for app
    
    * chore(app/gradle): change version style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,14 +13,14 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-early1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
